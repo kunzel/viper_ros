9e005a8a313e0dba2b33dc1905e0fbb0b88a2ad9
Jay <jay.young@gmail.com>
2016-05-07 19:11:48 +0100

    added trigger services

--- src/viper_ros/shutdown.py
+++ src/viper_ros/shutdown.py
@@ -11,6 +11,8 @@ from actionlib import *
 from actionlib.msg import *
 from scitos_ptu.msg import PtuGotoAction,PtuGotoGoal
 
+from std_srvs.srv import Trigger
+
 class Shutdown(smach.State):
     """
     Shutdown
@@ -27,9 +29,11 @@ class Shutdown(smach.State):
 
     def execute(self, userdata):
 
+
+
         goal = PtuGotoGoal()
         goal.pan = 0.0
-        goal.tilt = 0.0 
+        goal.tilt = 0.0
         goal.pan_vel = 60
         goal.tilt_vel = 60
         rospy.loginfo("SET PTU: pan: %f tilt: %f", goal.pan, goal.tilt)
@@ -37,5 +41,13 @@ class Shutdown(smach.State):
         self.ptu_client.wait_for_result()
         rospy.loginfo("Reached ptu goal")
 
-        return 'succeeded'
+        # here we inform the world_modeling package that a sequence of observations has finished
+        # I'm doing this at the end here because it blocks while the world_modeling system does its processing
+        # which could take 5-10 seconds
+        try:
+            end_observations_trigger = rospy.ServiceProxy('/end_observations',Trigger)
+            end_observations_trigger()
+        except rospy.ServiceException, e:
+            rospy.logerr("Service call failed: %s"%e)
 
+        return 'succeeded'
